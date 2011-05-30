9ca7b287e5a9a2b850f7d1ee9eba2083fb0b7f17
Yann Rochard <yann.rochard@gmail.com>
2011-05-30 12:38:34 +0200

    Ajout de la page surf.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def index
-    @title= "Bienvenue"
+    @title= "Beg an Dorchenn"
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Torche::Application.routes.draw do
   # first created -> highest priority.
 
   # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
+  match 'surfers' => 'application#surf', :as => "surf"
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:

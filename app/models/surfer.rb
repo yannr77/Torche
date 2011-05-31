661de7f84b880b06d7d45e8eb6fb6233ad793f3e
Yann Rochard <yann.rochard@gmail.com>
2011-05-31 15:53:44 +0200

    Ajout de email dans le form, l'index, le show + validation pour email, name et firstname

--- app/models/surfer.rb
+++ app/models/surfer.rb
@@ -1,2 +1,8 @@
 class Surfer < ActiveRecord::Base
+  
+    validates :email, :presence => true
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+    
+    validates :name, :presence => true
+    validates :firstname, :presence => true
 end

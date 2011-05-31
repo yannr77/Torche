965e3861f1fc2bd310f82994d59cbcb2838a11d5
Yann Rochard <yann.rochard@gmail.com>
2011-05-31 11:17:51 +0200

    Ajout de l'heure et du nom du visiteur

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
+  before_filter :set_time, :get_name
+
   def index
     @title = "Beg an Dorchenn"
   end
@@ -12,4 +14,16 @@ class ApplicationController < ActionController::Base
   def photos
     @title = "Photos"
   end
+
+
+  protected 
+
+  def get_name
+    @name = params[:name]
+  end
+  
+  def set_time
+    @time = Time.now.strftime("%d-%m-%Y %H:%M")
+  end
+
 end

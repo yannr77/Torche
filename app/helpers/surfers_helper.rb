ba88dd44f0badfb796599cc1d7702ae8875eb15b
Yann Rochard <yann.rochard@gmail.com>
2011-06-06 12:45:19 +0200

    Création d'une méthode def input_boards dans le helper de surfer

--- app/helpers/surfers_helper.rb
+++ app/helpers/surfers_helper.rb
@@ -1,2 +1,8 @@
 module SurfersHelper
+  
+  def input_checkbox_boards board, surfer
+  check = board.surfers.include?(surfer) ? "checked='checked'" : ""
+  	"<input type='checkbox' name='boardsss[#{board.id}]' #{check} />".html_safe
+  end
+  
 end

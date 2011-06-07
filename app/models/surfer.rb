3892be72c24b9950328cedcf9a5e99a04d3602e6
Yann Rochard <yann.rochard@gmail.com>
2011-06-07 13:19:03 +0200

    Modification de l'expression reguliere pour le tel suite au test tel

--- app/models/surfer.rb
+++ app/models/surfer.rb
@@ -1,6 +1,7 @@
 class Surfer < ActiveRecord::Base
+
     belongs_to :state
-  
+
     has_many :board_surfers
     has_many :boards, :through =>  :board_surfers
     
@@ -9,6 +10,6 @@ class Surfer < ActiveRecord::Base
     validates :email, :presence => true
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
     validates :tel,  :presence => true
-    validates_format_of :tel, :with => /0\d{1}\.?(\d{2}\.?){4}/
+    validates_format_of :tel, :with => /0\d{1}[\.\-\ \\]?(\d{2}[\.\-\ \\]?){4}/
 
 end

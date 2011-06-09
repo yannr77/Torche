23bde3e665f857d7eed95a594d538131b1d38763
Yann Rochard <yann.rochard@gmail.com>
2011-06-09 11:36:13 +0200

    Modification modelsurfer suppression attr_accessible: email

--- app/models/surfer.rb
+++ app/models/surfer.rb
@@ -5,13 +5,15 @@ class Surfer < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+attr_accessible  :password, :password_confirmation, :remember_me
 
+#LIAISONS
     belongs_to :state
-
     has_many :board_surfers
     has_many :boards, :through =>  :board_surfers
-    
+
+#FORMULAIRE
+    #surfer
     validates :name, :presence => true
     validates :firstname, :presence => true
     validates :email, :presence => true
@@ -19,7 +21,7 @@ class Surfer < ActiveRecord::Base
     validates :tel,  :presence => true
     validates_format_of :tel, :with => /0\d{1}[\.\-\ \\]?(\d{2}[\.\-\ \\]?){4}/
 
-
+    #photo surfer
     has_attached_file :photo, :styles => { :small => "150x150>" }
     validates_attachment_presence :photo
     validates_attachment_size :photo, :less_than => 5.megabytes
